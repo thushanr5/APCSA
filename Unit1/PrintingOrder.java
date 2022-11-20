31dc26dc209b5ab3f526a4ec0ce1a46c3163bdd9
thushanr5 <88255435+thushanr5@users.noreply.github.com>
2022-11-20 15:11:59 -0500

    Create PrintingOrder.java

---
+++ Unit1/PrintingOrder.java
@@ -0,0 +1,21 @@
+public class Variables {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi there");
+		System.out.print("joe");
+		System.out.print("r u ");
+		System.out.println("a");
+		System.out.println("a");
+		System.out.println("real");
+		System.out.print("person\n");
+    	
+		System.out.println("hi");
+		System.out.print("hello");
+		System.out.print("hola");
+		System.out.println("ni hao");
+		System.out.print("how are you");
+		System.out.println("hey");
+	}
+
+}
