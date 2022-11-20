133495a93c3b577043644b89dee03e85c7f6319e
thushanr5 <88255435+thushanr5@users.noreply.github.com>
2022-11-20 13:10:49 -0500

    Create Variables.java
    Variables in Java(Int, Double, String, Boolean)

---
+++ Unit1/Variables.java
@@ -0,0 +1,23 @@
+public class Variables {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Strings 
+		final String name = "Thushan";
+		String nameTwo = "John";
+		
+		//Int
+		int age = 20;
+		int ageTwo = 30;
+	
+		//Boolean
+		boolean isCool = true;
+		boolean isFriendly = false;
+		
+		Double val = 2.0;
+		Double strength = 15.5;
+		
+	}
+
+}
