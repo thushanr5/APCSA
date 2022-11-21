38e5ec46a158fc2f3f986c129f630e10b8dee5d1
thushanr5 <88255435+thushanr5@users.noreply.github.com>
2022-11-20 21:25:01 -0500

    Create TruthTables.java

---
+++ Unit1/TruthTables.java
@@ -0,0 +1,28 @@
+public class Variables {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Truth Table
+		
+		/*And Statements
+		 * T & T = T
+		 * T & F = F
+		 * F & T = F
+		 * F & F = F
+		 */
+		
+		/*Or Statements
+		 * T || T = T
+		 * T || F = T
+		 * F || T = T
+		 * F || F = F
+		 */
+		
+		/*Negation Statements
+		 * !T = F
+		 * !F = T
+		 */
+		
+	}
+
+}
