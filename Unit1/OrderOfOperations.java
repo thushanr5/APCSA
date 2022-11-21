01af1e930fc077a5603c0c12744d4df022040adc
thushanr5 <88255435+thushanr5@users.noreply.github.com>
2022-11-20 21:08:03 -0500

    Create OrderOfOperations.java

---
+++ Unit1/OrderOfOperations.java
@@ -0,0 +1,16 @@
+public class Variables {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(5%2+3*2+5+(3/8)-2);
+		// 5 % 2 == 1
+		// 1 + 6 = 7
+		//12 + (3/8) = 12 - 2 = 10
+		
+		int x = 3/5 + (2*4)+7%5/3;
+		System.out.println(x);
+		//8
+    
+	}
+
+}
