11b636f06ef97c584c649e23864d9ce5bdafe9f8
thushanr5 <88255435+thushanr5@users.noreply.github.com>
2022-11-20 21:32:46 -0500

    Create DemorganLaw.java

---
+++ Unit1/DemorganLaw.java
@@ -0,0 +1,19 @@
+public class Variables {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		/*
+		!(A && B) is the same as !A || !B
+		!(A || B) is the same as !A && !B
+		!(C > D) is the same as C <= D
+		!(C < D) is the same as C >= D
+		!(C >= D) is the same as C < D
+		!(C <= D) is the same as C > D
+		!(E == F) is the same as E != F
+		!(E != F) is the same as E == F
+		*/
+		
+	}
+
+}
