d5605a31aab07f1023306947ec76f83bfc091442
thushanr5 <88255435+thushanr5@users.noreply.github.com>
2022-11-26 14:07:07 -0500

    Create mathclass.java

---
+++ Unit1/mathclass.java
@@ -0,0 +1,23 @@
+import java.math.*;
+public class Lesson1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double x = Math.pow(2, 2);  //returns a double value naturally if not need to cast
+		double y = Math.abs(5); // Returns a double value
+		int z = Math.abs(1); //Returns an int value
+		double g = Math.abs(1); //Returns a double value
+		double sQ = Math.sqrt(4); //Returns a double value
+		double ppW = Math.pow(2, 3); //Returns a double value
+		double tupple = Math.random(); //Returns a double value
+		
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(z);
+		System.out.println(g);
+		System.out.println(sQ);
+		System.out.println(ppW);
+		System.out.println(tupple);
+	}
+
+}
